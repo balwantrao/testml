5d7565541a78aca5c5c8059980eb33a4c03b5ef8
balwantrao <106421117+balwantrao@users.noreply.github.com>
2023-08-30 20:43:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Test Project",page_icon="::",layout="wide")
+
+
+st.subheader ("Hello to this test Project!")
+st.title("test Project to check whether project is running on browser or not.")
+st.write("This page showing means This page is working fine.")
+
